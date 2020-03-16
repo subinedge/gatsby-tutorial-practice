91c314924abf372c308789f2eec375f6d1bbe0fe
Subin <subinsamrat96@gmail.com>
2020-03-16 21:55:18 +0530

    env variables added

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+ require('dotenv').config({
+   path: `.env.${process.env.NODE_ENV}`,
+ })
+
 module.exports = {
   /* Your site config here */
   siteMetadata:{
@@ -17,6 +21,13 @@ module.exports = {
     options:{
       name:'images',
       path:`${__dirname}/src/images`
+    }
+  },
+  {
+    resolve:'gatsby-source-contentful',
+    options:{
+      spaceId: process.env.CONTENTFUL_ID,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
     }
   },
   `gatsby-transformer-sharp`, 
